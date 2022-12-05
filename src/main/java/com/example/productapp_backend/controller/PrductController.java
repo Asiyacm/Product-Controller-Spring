a612944a1e7d4549cb228f71f70cda457726b8df
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-05 20:35:00 +0530

    search commit

--- src/main/java/com/example/productapp_backend/controller/PrductController.java
+++ src/main/java/com/example/productapp_backend/controller/PrductController.java
@@ -23,4 +23,12 @@ public class PrductController {
         return("Welcome to product Addpage");
 
     }
+
+    @GetMapping("/search")
+
+    public String ProductSearchPage(){
+
+        return("Welcome to product Searchpage");
+
+    }
 }
